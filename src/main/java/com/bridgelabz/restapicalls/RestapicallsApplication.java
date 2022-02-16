acff3f1f4cf43aebeae90f5528840e6aa896efb6
Ravi3599 <ravikantdukre2018@gmail.com>
2022-02-16 13:44:46 +0530

    ADDED: created rest controller and showed message

---
+++ src/main/java/com/bridgelabz/restapicalls/RestapicallsApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.restapicalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapicallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapicallsApplication.class, args);
+	}
+
+}
