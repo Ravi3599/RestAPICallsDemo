391e5c998c72aefb19a8990e4606d906653597bf
Ravi3599 <ravikantdukre2018@gmail.com>
2022-02-16 14:04:04 +0530

    UPDATED: rest call to show message passing firstName and lastName in body

--- src/main/java/com/bridgelabz/restapicalls/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/restapicalls/controller/HelloRestController.java
@@ -2,9 +2,13 @@ package com.bridgelabz.restapicalls.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.restapicalls.model.User;
 
 @RestController
 @RequestMapping("/web")
@@ -25,4 +29,8 @@ public class HelloRestController {
 		return "Hello "+name+" From BridgeLabz";
 	}
 	
+	@PostMapping("/post")
+	public String userData(@RequestBody User user) {
+		return "Hello "+user.getfName()+" "+user.getlName()+" from BridgeLabz";
+	}
 }
